537572b6d93bdc7e530ed7e7ea66ee2665801e53
Dimitris Pnevmatikos <pnevmatikos@gmail.com>
2020-02-03 11:02:36 +0200

    - Fix DbContext

--- TinyCrm.Core/Data/TinyCrmDbContext.cs
+++ TinyCrm.Core/Data/TinyCrmDbContext.cs
@@ -6,9 +6,31 @@ using System.Threading.Tasks;
 
 using Microsoft.EntityFrameworkCore;
 
-namespace TinyCrm.Data
+namespace TinyCrm.Core.Data
 {
     public class TinyCrmDbContext : DbContext
     {
+        private readonly string connectionString_;
+
+        public TinyCrmDbContext(string connString)
+        {
+            connectionString_ = connString;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Entity<Model.Customer>()
+                .ToTable("Customer", "core");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(connectionString_);
+        }
     }
 }
